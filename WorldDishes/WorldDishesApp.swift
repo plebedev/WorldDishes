01cf837cb384b62ed60efdbaed6afb4f43477ab3
Peter Lebedev <plebedev@jobcase.com>
2024-07-31 07:15:02 -0400

    Initial Commit

---
+++ WorldDishes/WorldDishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldDishesApp.swift
+//  WorldDishes
+//
+//  Created by Peter Lebedev on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct WorldDishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
